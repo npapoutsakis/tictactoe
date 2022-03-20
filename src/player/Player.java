f4d729fdc652a7c984aaa18d79f1e3f148e20f80
Nikolaos Papoutsakis <nikospapoutsak@gmail.com>
2022-03-20 22:47:39 +0200

    Player Details Added

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,16 +1,43 @@
 package player;
 
 public class Player {
-	String name;
-	int games_played;
-	
-	public Player() {
-		
+	private String name;
+	private int games_played;
+	private int wins; 
+	public Player(String name, int games, int wins) {
+		this.name = name;
+		this.games_played = games;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getGames_played() {
+		return games_played;
+	}
+
+	public void setGames_played(int games_played) {
+		this.games_played = games_played;
+	}
+
+	public int getWins() {
+		return wins;
+	}
+
+	public void setWins(int wins) {
+		this.wins = wins;
 	}
 	
-	
-	
-	
+	public void printPlayerInfo() {
+		System.out.println("Name: "+this.name);
+		System.out.println("Games: "+this.games_played);
+		System.out.println("Wins: "+this.wins);
+	}
 	
 	
 }
